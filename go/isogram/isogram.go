388c1f7f56c8b9274d7a5de432bafaf73c6f65aa
Dr. Gernot Starke <gs@gernotstarke.de>
2020-12-22 17:39:47 +0100

    added isogram (might be optimized lager)

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,22 @@
 package isogram
+
+import "strings"
+
+func IsIsogram(word string) bool {
+	stats := make(map[string]int)
+
+	// remove "-" and " " from word
+	word = strings.ReplaceAll(word, "-", "")
+	word = strings.ReplaceAll(word, " ", "")
+
+	iso := true
+	for _, c := range word {
+		upC := strings.ToUpper(string(c))
+		stats[string(upC)]++
+		if stats[string(upC)] > 1 {
+			iso = false
+		}
+	}
+
+	return iso
+}
